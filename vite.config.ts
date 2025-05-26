d4fa499c63744422b1b684e57a059f8bd7dcb6ff
CoCo9122 <banaji.yu0517@outlook.jp>
2025-05-26 10:38:52 +0900

    初期構築

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label'
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } }), tsconfigPaths()],
+  base: '/ht-sb',
+  server: {
+    watch: {
+      usePolling: true, // ← 変更検知に polling を使う（Dockerでは必須）
+    },
+  }
+})
