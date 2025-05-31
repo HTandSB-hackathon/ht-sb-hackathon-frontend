cac70e17a83e2bfde38c72905f177bf09866675f
CoCo9122 <banaji.yu0517@outlook.jp>
2025-06-01 01:48:10 +0900

    ルート関係の編集

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import CharacterPage from "@/components/pages/CharacterPage";
 import ChatsPage from "@/components/pages/ChatsPage";
 import HomePage from "@/components/pages/HomePage";
 import LoginPage from "@/components/pages/LoginPage";
-import MainPage from "@/components/pages/MainPage";
 import RegisterPage from "@/components/pages/RegisterPage";
 import TutorialPage from "@/components/pages/TutorialPage";
 import LandingPage from "./components/pages/LandingPage";
@@ -15,13 +14,10 @@ function App() {
 	return (
 		<BrowserRouter basename="/ht-sb">
 			<Routes>
-				<Route path="/" element={<MainPage />} />
-
 				<Route path="" element={<LandingPage />} />
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/register" element={<RegisterPage />} />
 				<Route path="/tutorial" element={<TutorialPage />} />
-				<Route path="/home" element={<HomePage />} />
 				<Route path="/chats" element={<ChatsPage />} />
 				<Route path="/chats/:characterId" element={<CharacterPage />} />
 
@@ -32,7 +28,7 @@ function App() {
 				{/* ルートが存在しない場合の404ページ */}
 				{/* 認証が必要なルート */}
 				<Route element={<ProtectedRoute />}>
-					<Route path="/main" element={<MainPage />} />
+					<Route path="/home" element={<HomePage />} />
 				</Route>
 			</Routes>
 		</BrowserRouter>
